a8bd80f6c472297464db523680599a0fa6f0c64c
Popkov Anton <popkow.anton@gmail.com>
2016-12-19 12:16:59 +0700

    На кнопку добавлен обработчик звука (класс SoundPool)

--- app/src/main/java/com/jaydevelopment/popkovanton/vietnamesephrasebook/DateandtimeActivity.java
+++ app/src/main/java/com/jaydevelopment/popkovanton/vietnamesephrasebook/DateandtimeActivity.java
@@ -1,22 +1,47 @@
 package com.jaydevelopment.popkovanton.vietnamesephrasebook;
 
 import android.content.Intent;
+import android.media.AudioManager;
+import android.media.SoundPool;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.ImageButton;
 
 import com.google.android.gms.ads.AdRequest;
 import com.google.android.gms.ads.AdView;
 
-public class DateandtimeActivity extends AppCompatActivity {
+import java.io.IOException;
 
+
+public class DateandtimeActivity extends AppCompatActivity implements View.OnClickListener {
+
+    ImageButton button;
+
+    final int MAX_STREAMS = 5;
+
+
+    private SoundPool sp;
+    private int soundWhatsTimeIsIt;
     private AdView mAdView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.dateandtime);
+
+        sp = new SoundPool(MAX_STREAMS, AudioManager.STREAM_MUSIC, 0); //создаем класс SoundPool
+
+        try {                                                                     //
+            soundWhatsTimeIsIt = sp.load(getAssets().openFd("dateandtime/whats_time_is_it.wav"), 1);   //
+        } catch (IOException e) {                                                 //
+            e.printStackTrace();                                                  //  загрузка из assets
+        }                                                                         //
+
+        button = (ImageButton) findViewById(R.id.imageButton);
+        button.setOnClickListener(this);
 
         mAdView = (AdView) findViewById(R.id.adView10);
         AdRequest adRequest = new AdRequest.Builder().build();
@@ -39,6 +64,15 @@ public class DateandtimeActivity extends AppCompatActivity {
         }
     }
 
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.imageButton:
+                sp.play(soundWhatsTimeIsIt, 1, 1, 0, 0, 1);
+                break;
+        }
+    }
+
     protected void onResume() {
         mAdView.resume();
 
@@ -56,4 +90,5 @@ public class DateandtimeActivity extends AppCompatActivity {
         mAdView.destroy();
         super.onDestroy();
     }
+
 }
